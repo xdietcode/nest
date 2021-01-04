015bd976ed6539b2e849e9d5ab5a06d1ce1e1901
xdietcode <xdietcode@gmail.com>
2021-01-04 14:07:40 -0800

    handle localStorage for login and logout

--- nest-web/src/components/App.js
+++ nest-web/src/components/App.js
@@ -4,15 +4,21 @@ import Main from "./Main";
 import { TOKEN_KEY } from "../constants";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [isLoggedIn, setIsLoggedIn] = useState(
+        // check current isLoggedIn state
+        localStorage.getItem( TOKEN_KEY ) ? true : false
+    )
 
     const logout = () => {
         setIsLoggedIn( false);
+        // remove token
+        localStorage.removeItem(TOKEN_KEY);
     }
 
     // save token to localStorage
     const loggedIn = token => {
         localStorage.setItem(TOKEN_KEY,token);
+        setIsLoggedIn(true);
     }
 
     return (
