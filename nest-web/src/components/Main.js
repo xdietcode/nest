a9922e1369a30c54b97a96b2aeb746dc737e94e2
xdietcode <xdietcode@gmail.com>
2021-01-05 13:57:47 -0800

    fix typo

--- nest-web/src/components/Main.js
+++ nest-web/src/components/Main.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router';
+import {Switch, Route, Redirect} from 'react-router';
 
 import Login from './Login';
 import Register from './Register';
 import Home from './Home';
 
 function Main(props) {
-    const { isLoggedIn, handleLoggedIn } = props;
+    const {isLoggedIn, handleLoggedIn} = props;
     const showLogin = () => {
         // case 1: logged in
         // case 2: not logged in
-        return isLoggedIn ? <Redirect to="/home" /> : <Login handleLoggedIn={handleLoggedIn}/>
+        return isLoggedIn ? <Redirect to="/home"/> : <Login handleLoggedIn={handleLoggedIn}/>
 
     }
 
     // whether show home
     const showHome = () => {
-        return isLoggedIn ? <Home /> : <Redirect to="/login" />
+        return isLoggedIn ? <Home/> : <Redirect to="/login"/>
     }
 
     return (
         <div className="main">
-        <Switch>
-            <Route path={"/login"} render={showLogin} />
-            <Route path={"/register"} component={Register}/>
-            <Route path={"/home"} render={showHome}/>
-        </Switch>
+            <Switch>
+                <Route path={"/login"} render={showLogin}/>
+                <Route path={"/register"} component={Register}/>
+                <Route path={"/home"} render={showHome}/>
+            </Switch>
         </div>
     );
 }
