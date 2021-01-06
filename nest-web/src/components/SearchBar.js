79b8f588555d6ecbd1b7c771e2fb0d4153e5c516
xdietcode <xdietcode@gmail.com>
2021-01-05 21:09:08 -0800

    add search logic

--- nest-web/src/components/SearchBar.js
+++ nest-web/src/components/SearchBar.js
@@ -6,24 +6,35 @@ const {Search} = Input;
 
 function SearchBar(props) {
 
-    const handleSearch = value => {
+    const [searchType, setSearchType] = useState(SEARCH_KEY.all);
+    const [error, setError] = useState(null);
+
+
+
+    // case 1: type === all -> inform Home to fetch data
+    const changeSearchType = e => {
+        console.log('radio checked', e.target.value);
+        setSearchType(e.target.value);
+        setError("");
+        // if clicked on ALl, inform HOME to fetch data
+        if (searchType === SEARCH_KEY.all) {
+            props.handleSearch( { type: SEARCH_KEY.all, keyword: ""})
+        }
+    };
+
+    // case2: keyword/user + input
+    const handleClickSearch = value => {
         console.log(value);
         // error case
         if (value === "" && searchType !== SEARCH_KEY.all) {
             setError("Please input search keyword");
             return;
         }
-        setError("");
+        // do search
+        // inform HOME to fetch data
+        // -> pass search word + type => Home props.fn
+        props.handleSearch({type: searchType, keyword: value})
     }
-
-    const [searchType, setSearchType] = useState(SEARCH_KEY.all);
-    const [error, setError] = useState(null);
-
-    const changeSearchType = e => {
-        console.log('radio checked', e.target.value);
-        setSearchType(e.target.value);
-        setError("");
-    };
 
     return (
         <div className="search-bar">
@@ -34,7 +45,7 @@ function SearchBar(props) {
                 enterButton="Search"
                 size="large"
                 disabled={searchType === SEARCH_KEY.all}
-                onSearch={handleSearch}
+                onSearch={handleClickSearch}
             />
             <p className="error-msg"> {error} </p>
             <Radio.Group onChange={changeSearchType} value={searchType}>
