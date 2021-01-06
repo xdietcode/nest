0326e016b5d3a34ae4d775e1d3739f5bb3941089
xdietcode <xdietcode@gmail.com>
2021-01-05 21:09:46 -0800

    change logo for Banner

--- nest-web/src/components/TopBar.js
+++ nest-web/src/components/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/logo2.svg";
 import { LogoutOutlined } from "@ant-design/icons";
 
 function TopBar(props) {
